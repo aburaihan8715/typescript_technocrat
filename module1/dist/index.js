4491719352b55e600e2fc5dfee7493f9ec5049e6
aburahan8721 <aburaihan8721@gmail.com>
2024-05-01 19:30:03 +0600

    module1 done

---
+++ module1/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+{
+    // start block
+    let course = "Next Level Web Development";
+    console.log(course);
+    // end block
+}
---
+++ module1/src/index.ts
@@ -0,0 +1,8 @@
+{
+  // start block
+
+  let course = "Next Level Web Development";
+  console.log(course);
+
+  // end block
+}

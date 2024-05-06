7fce983feb503c56b03a90d7d1f5e62402e24bfe
aburahan8721 <aburaihan8721@gmail.com>
2024-05-06 20:51:22 +0600

    update done

---
+++ module2/dist/2.11.js
@@ -0,0 +1,17 @@
+"use strict";
+{
+    const person1 = {
+        name: "Mr. XY",
+        age: 200,
+        contactNo: "017",
+    };
+    const EmptyObj = {};
+    // const obj1: MyObj = {
+    //   a: "aa",
+    //   b: "bb",
+    //   c: "cc",
+    //   d: "dd",
+    //   e: 6,
+    // };
+    //
+}
